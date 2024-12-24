168a569afdeb16d624b1d653d9610c10b4f9bc3a
disharun <vishdisha005@gmail.com>
2024-12-24 23:01:15 +0530

    first commit

---
+++ admin/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+import logo2 from "../../assets/logo2.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img className="logo" src={logo2} alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
